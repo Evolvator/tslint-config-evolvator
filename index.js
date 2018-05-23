e2c9bead2041428ea03297098b7dd72daf8f803f
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-05-23 15:03:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "extends": [
+    "tslint-eslint-rules",
+    "tslint-config-airbnb"
+  ],
+  "rules": {
+    "prefer-const": false, // bug in for usage
+    "no-increment-decrement": false, // sometimes it is necessary, prohibitions do not lead to good
+    "no-unused-variable": true, // todo: import too
+    "no-var-keyword": false, // 2.2 in practice, interference
+    "function-name": false, // 23.1 is good, but... the rest is dictate
+    "max-line-length": false, // 13.12 just no
+    "import-name": false, // 23.6 just no
+    "one-variable-per-declaration": false, // 13.2 is good, but painfully for experiments
+    "variable-name": [ // 23.2 just no, sometimes it is necessary, private variables and so on
+      true,
+      "ban-keywords",
+      "check-format",
+      "allow-leading-underscore",
+      "allow-trailing-underscore",
+      "allow-pascal-case",
+      "allow-snake-case"
+    ]
+  }
+};
---
+++ readme.md
@@ -0,0 +1,27 @@
+# tslint-config-evolvator
+
+Our TSLint config based on Airbnb JavaScript Style.
+
+[![NPM](https://img.shields.io/npm/v/ancient-cursor.svg)](https://www.npmjs.com/package/ancient-cursor)
+
+- [tslint-config-evolvator](#tslint-config-evolvator)
+  - [Install](#install)
+  - [Usage](#usage)
+  - [Rules](#rules)
+
+## Install
+
+```sh
+npm install tslint-config-evolvator
+```
+
+## Usage
+
+```json
+{
+  "extends": "tslint-config-evolvator"
+}
+```
+
+## Rules
+- [tslint-config-airbnb](https://github.com/progre/tslint-config-airbnb)
